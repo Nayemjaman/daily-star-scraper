9f77e489d6bbca1b5ccb1aebb436d59f9430566f
Nayem Jaman Tusher <njaman26@gmail.com>
2022-01-02 23:27:12 +0600

    scraping news picture

--- scraper/scraper/spiders/dailystarspider.py
+++ scraper/scraper/spiders/dailystarspider.py
@@ -10,7 +10,8 @@ class DailystarspiderSpider(scrapy.Spider):
     def parse(self, response):
         headline = response.css('h1::text').get()
         date = response.css('.text-10::text').get()
+        images = response.css('.section-media img::attr(data-srcset)').extract()
         news_1 = response.css('p strong::text').extract()
         news_2 = response.css('.section-content p::text').extract()
         news_detail = news_1 + news_2
-        yield {'headline':headline,'date':date, 'news_detail':news_detail}
+        yield {'headline':headline, 'images':images, 'date':date, 'news_detail':news_detail}
