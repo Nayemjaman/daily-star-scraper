062b268702169deaaa89bf7eda82fae5b471a56f
Nayem Jaman Tusher <njaman26@gmail.com>
2022-01-04 02:24:51 +0600

    collect news url

--- scraper/scraper/spiders/dailystarspider.py
+++ scraper/scraper/spiders/dailystarspider.py
@@ -23,7 +23,9 @@ class DailystarspiderSpider(scrapy.Spider):
 
     def news_pages(self, response): #categories news links 
         news_links = response.css('h3.title a::attr(href)').extract()
-        yield {'news_links':news_links}
+        news_links = ['https://www.thedailystar.net' + x for x in news_links][:1]
+        # yield {'news_links':news_links}
+        yield from response.follow_all(news_links, self.news_details)
 
 
 
@@ -33,13 +35,14 @@ class DailystarspiderSpider(scrapy.Spider):
     #     yield from response.follow_all(news_links, self.news_details)
     #     # yield {'news_links':news_links}
 
-    # def news_details(self, response):
-    #     headline = response.css('h1::text').extract()
-    #     # headline =  ''.join(headline) or use get()
-    #     date = response.css('.text-10::text').extract()
-    #     images = response.css(
-    #         '.section-media img::attr(data-srcset)').extract()
-    #     news_1 = response.css('p strong::text').extract()
-    #     news_2 = response.css('.section-content p::text').extract()
-    #     news_detail = news_1 + news_2
-    #     yield {'headline': headline, 'images': images, 'date': date, 'news_detail': news_detail}
+    def news_details(self, response):
+        current_url = response.request.url
+        headline = response.css('h1::text').extract()
+        # headline =  ''.join(headline) or use get()
+        date = response.css('.text-10::text').extract()
+        images = response.css(
+            '.section-media img::attr(data-srcset)').extract()
+        news_1 = response.css('p strong::text').extract()
+        news_2 = response.css('.section-content p::text').extract()
+        news_detail = news_1 + news_2
+        yield {'current_url':current_url,'headline': headline, 'images': images, 'date': date, 'news_detail': news_detail}
