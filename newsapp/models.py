b7409f9dd31f726e9789e7959ef6cddc7e1a1bc6
Nayem Jaman Tusher <njaman26@gmail.com>
2022-01-06 23:58:25 +0600

    creating models in models.py

--- newsapp/models.py
+++ newsapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class newsdetails(models.Model):
+    current_url = models.CharField(max_length=150)
+    categories = models.CharField(max_length=150)
+    sub_categories = models.CharField(max_length=150)
+    headline = models.CharField(max_length=150)
+    date = models.CharField(max_length=150)
+    images = models.CharField(max_length=150)
+    news_detail = models.CharField(max_length=150)
