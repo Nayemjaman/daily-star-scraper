43533e446d4c643e1976721f9208354e6c34c539
Nayem Jaman Tusher <njaman26@gmail.com>
2022-01-07 00:03:57 +0600

    items.py

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -4,9 +4,10 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy_djangoitem import DjangoItem
+from newsapp.models import newsdetails
 
-
-class ScraperItem(scrapy.Item):
+class ScraperItem(DjangoItem):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    django_model = newsdetails
