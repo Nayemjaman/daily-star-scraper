0d5eb3622cdbb9bc438ff77fcb69c9ce59856bbf
Nayem Jaman Tusher <njaman26@gmail.com>
2022-01-07 01:22:37 +0600

    piplines

---
+++ scraper/management/commands/crawl.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from scraper.scraper.spiders import dailystarspider
+
+class Command(BaseCommand):
+    help = "Release the spiders"
+
+    def handle(self, *args, **options):
+        process = CrawlerProcess(get_project_settings())
+
+        process.crawl(dailystarspider)
+        process.start()
